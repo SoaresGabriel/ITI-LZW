c8b766846f52d52fe566e1a78c46929f027d5739
Gabriel Soares <soaresggm@gmail.com>
2018-04-18 17:43:50 -0300

    Added DecompressTree implementation

---
+++ DecompressTree.cpp
@@ -0,0 +1,50 @@
+#include <stdexcept>
+#include "DecompressTree.h"
+
+char DNode::getFirstByte() {
+    if(parent == nullptr) {
+        return static_cast<char>(byte);
+    } else {
+        return parent->getFirstByte();
+    }
+}
+
+void DNode::writeStringIn(std::ofstream& file) {
+    if(parent != nullptr) parent->writeStringIn(file);
+    file.put(static_cast<char>(byte));
+}
+
+DecompressTree::DecompressTree(unsigned long sizeLimit) : SIZE_LIMIT(sizeLimit) {
+    for(size = 0; size < 256; size++) {
+        dictionary.push_back(new DNode(nullptr, static_cast<int>(size)));
+    }
+    dictionary.push_back(new DNode(nullptr, -1)); // EOF
+    size++;
+
+    bitsForIndex = 9;
+    nextBitIncrease = 1u << bitsForIndex;
+
+}
+
+DNode *DecompressTree::newChildNode(unsigned long parent) {
+    if(size < SIZE_LIMIT) {
+        auto node = new DNode(dictionary[parent]);
+
+        dictionary.push_back(node);
+        size++;
+
+        if(size > nextBitIncrease) {
+            bitsForIndex++;
+            nextBitIncrease <<= 1u; // * 2
+        }
+
+        return node;
+    } else {
+        return nullptr;
+    }
+
+}
+
+DNode *DecompressTree::operator[](size_t index) {
+    return dictionary[index];
+}
---
+++ DecompressTree.h
@@ -0,0 +1,39 @@
+#ifndef LZW_DECOMPRESSTREE_H
+#define LZW_DECOMPRESSTREE_H
+
+#include <vector>
+#include <fstream>
+
+struct DNode {
+
+    DNode* parent;
+    int byte;
+
+    DNode(DNode* parent, int byte) : parent(parent), byte(byte) {}
+    explicit DNode(DNode* parent) : parent(parent), byte(-1) {}
+
+    char getFirstByte();
+    void writeStringIn(std::ofstream& file);
+};
+
+class DecompressTree {
+private:
+
+    unsigned long size;
+    unsigned long SIZE_LIMIT;
+    unsigned long nextBitIncrease;
+public:
+
+    std::vector<DNode *> dictionary;
+    explicit DecompressTree(unsigned long sizeLimit);
+
+    unsigned int bitsForIndex;
+
+    DNode* newChildNode(unsigned long parent);
+
+    DNode* operator[](size_t index);
+
+};
+
+
+#endif //LZW_DECOMPRESSTREE_H
